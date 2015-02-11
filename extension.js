f266f89d9117cfd56b497f55a1585f626cd4cf29
DJZodiac <zodiacthedj@gmail.com>
2015-02-11 13:10:56 -0500

    Update extension.js

--- extension.js
+++ extension.js
@@ -55,7 +55,7 @@
     localStorage.setItem("basicBotsettings", JSON.stringify({
         botName: "IFeelFreeBot",
         language: "english",
-        chatLink: "https://rawgit.com/Yemasthui/basicBot/master/lang/en.json",
+        chatLink: "https://rawgit.com/DJZodiac/basicBot/master/lang/en.json",
         startupCap: 10, // 1-200
         startupVolume: 20, // 0-100
         startupEmoji: true, // true or false
@@ -102,12 +102,12 @@
         songstats: true,
         commandLiteral: "!",
         blacklists: {
-            NSFW: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
-            OP: "https://rawgit.com/Yemasthui/basicBot-customization/master/blacklists/ExampleOPlist.json"
+            NSFW: "https://rawgit.com/DJZodiac/basicBot-customization/master/blacklists/ExampleNSFWlist.json",
+            OP: "https://rawgit.com/DJZodiac/basicBot-customization/master/blacklists/ExampleOPlist.json"
         }
     }));
 
     //Start the bot and extend it when it has loaded.
-    $.getScript('https://rawgit.com/Yemasthui/basicBot/master/basicBot.js', extend);
+    $.getScript('https://rawgit.com/DJZodiac/basicBot/master/basicBot.js', extend);
 
 }).call(this);
